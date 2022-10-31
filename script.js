fce1cbeef040c50be55f7a1e9ee5f5019955ec51
Max Tribble <maxtribble@live.com>
2022-10-30 19:09:52 -0600

    adding the function to concat a new array from true/false

--- script.js
+++ script.js
@@ -10,34 +10,49 @@ var symbols = window.confirm ("Do you want symbols?");
 var numbers = window.confirm ("Do you want numbers?");
 //invasive pop up 
 // Get references to the #generate element  
-
-if (characters < 8) {
-    window.alert ("Characters need to be between 8-128");
+function validate() {
+  
+    
+  
+  window.alert ("Characters need to be between 8-128");
     var characters  =  window.prompt ("How many characters");
-    } 
-if (characters > 128) {
+    return arguments.callee;
+} 
+for (
+  {if (typeof parseInt(characters) === typeof 5) { window.alert ("Characters need to be between 8-128");
+  var characters  =  window.prompt ("How many characters");}} 
+  else {
+ if (characters > 128) {
   window.alert ("Characters need to be between 8-128");
   var characters  =  window.prompt ("How many characters");
-    } 
+ }})
+//if (characters > 128) {
+  //window.alert ("Characters need to be between 8-128");
+  //var characters  =  window.prompt ("How many characters");
+    //} 
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   let component = noCap
   if (capitol) component = component.concat(cap)
   if (symbols) component = component.concat(symbol)
   if (numbers) component = component.concat(number)
-  
-  var passwordCharacters = []
-  for (let i = 0; i < characters, i++;) {
 
+  var passwordCharacters = []
+
+  for (let i = 0; i < characters; i++) {
+    var passwordS = component[Math.floor(Math.random() * component.length)]
+    passwordCharacters.push(passwordS)
+    //return passwordCharacters.String("")
   }
-    console.log(component);
+  console.log(passwordCharacters.join(""))
+    
     console.log(capitol);
     console.log(symbol);
   //prompts for criteria
   //8-128 characters , lower case , upper case , numbers , special
   //validate it's within parameters
   //display password
-  return "generated password"
+  return passwordCharacters.join("")
 }
 // Write password to the #password input
 function writePassword() {
