5b2b59908f7f3664cf9fa3c3cbed4341e97299bd
Max Tribble <maxtribble@live.com>
2022-10-31 22:06:04 -0600

    adding comentary, removing intial psuedo code and removing testing console logs

--- script.js
+++ script.js
@@ -1,71 +1,53 @@
-// Assignment code here
-//array of letters and numbers 
+//variable arrays to grab the password elements from
 var number = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 0]
 var symbol = ["!" , "@" , "#" , "$" , "%" , "&" , "*"]
 var cap = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"]
-var noCap = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"] 
-var characters  =  window.prompt ("How long do you want your password to be? (Value from 8-128)");
+var noCap = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+
+//window prompt to get the length of password
+var characters = window.prompt ("How long do you want your password to be? (Value from 8-128)");
+
+//confirming wheather they want certain elements in their password with window pop ups
 var capitol = window.confirm ("Do you want capitol letters?");
 var symbols = window.confirm ("Do you want symbols?");
 var numbers = window.confirm ("Do you want numbers?");
-//invasive pop up 
-// Get references to the #generate element  
+
+ //turning characters variable into a number instead of the default(string)
 var characters = parseInt(characters)
-	
-while (isNaN(characters) || characters < 8 || characters > 128)//{;characters > 128; characters < 8) {
-    {console.log("working");
-      //window.alert("Not a valid number between 8-128.")
+
+//code running in a loop to make sure the user enters valid character
+while (isNaN(characters) || characters < 8 || characters > 128){
       var characters  =  window.prompt ("Input valid number between 8-128.");
     } 
-console.log(characters)
-  //for (){
-      //window.alert ("Not a valid number between 8-128.")
-    //  var characters  =  window.prompt ("Input valid number between 8-128.")}
-//var characters = parseInt(characters)
-      //for {
-      //window.alert ("Not a valid number between 8-128.");
-      //var characters  =  window.prompt ("Input valid number between 8-128.");
-    //}
 
-  
-//if (characters > 128) {
-  //window.alert ("Characters need to be between 8-128");
-  //var characters  =  window.prompt ("How many characters");
-    //} 
+//connects the generate button tag from css and html and turns it into a new variable
 var generateBtn = document.querySelector("#generate");
+
 function generatePassword() {
+
+  // starting with the non-capitol array, adding other arrays to it using concat, and making the concat list its own varible for later use
   let component = noCap
   if (capitol) component = component.concat(cap)
   if (symbols) component = component.concat(symbol)
   if (numbers) component = component.concat(number)
 
+//open array to put the random characters in to
   var passwordCharacters = []
 
+// random number generator to select a random item inside the array: component. Setting the maximum limit to the length of component to reach every item inside.
   for (let i = 0; i < characters; i++) {
-    var passwordS = component[Math.floor(Math.random() * component.length)]
-    passwordCharacters.push(passwordS)
-    //return passwordCharacters.String("")
+    var passwords = component[Math.floor(Math.random() * component.length)]
+    passwordCharacters.push(passwords) // pushing the randomized peices of the component array in to the empty array
   }
-  console.log(passwordCharacters.join(""))
-    
-    console.log(capitol);
-    console.log(symbol);
-  //prompts for criteria
-  //8-128 characters , lower case , upper case , numbers , special
-  //validate it's within parameters
-  //display password
-  return passwordCharacters.join("")
+  return passwordCharacters.join("") // returning the password characters with the join function to make it all in to one string with no seperation
 }
-// Write password to the #password input
+
+// setting the previous function that assembled our generated password to a variable, selecting the empty password line and displaying the assembled password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
+// event listener to detect a click on the generate button and call the write password function to display the new generated password
 generateBtn.addEventListener("click", writePassword);
-//characters
-//paste password in the generate password doc
